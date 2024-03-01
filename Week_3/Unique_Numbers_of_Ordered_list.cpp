7e6a72a4ff8587900813a472f44e29de67f8d4d2
Eman helal <helale782@gmail.com>
2024-03-01 22:31:29 +0200

    modifing file

--- Week_3/Unique_Numbers_of_Ordered_list.cpp
+++ Week_3/Unique_Numbers_of_Ordered_list.cpp
@@ -4,19 +4,20 @@ using namespace std;
 
 int main()
 {
-	int N, Arry[1000], value, pointer{ 0 };
+	int N, Arry[1000], value, pointer{ -1 };
 	cin >> N;
 	for (int i = 0; i < N;i++) {
 		cin >> value;
 		if (i == 0) {
 			Arry[i] = value;
-		}
-		else if (value != Arry[pointer]) {
-			Arry[pointer+1] = value;
 			pointer++;
 		}
+		else if (value != Arry[pointer]) {
+			pointer++;
+			Arry[pointer] = value;
+		}
 	}
-	for (int j = 0; j < pointer ;j++) {
+	for (int j = 0; j <= pointer ;j++) {
 		cout << Arry[j] << " ";
 	}
 }
