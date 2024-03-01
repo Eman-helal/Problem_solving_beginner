0741cefbaefed3737ce574391438a1317066e432
Eman Helal <154541050+Eman-helal@users.noreply.github.com>
2024-03-01 20:42:32 +0200

    Update Unique_Numbers_of_Ordered_list.cpp

--- Week_3/Unique_Numbers_of_Ordered_list.cpp
+++ Week_3/Unique_Numbers_of_Ordered_list.cpp
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 #include <iostream>
 
 using namespace std;
@@ -21,27 +20,3 @@ int main()
 		cout << Arry[j] << " ";
 	}
 }
-=======
-#include <iostream>
-
-using namespace std;
-
-int main()
-{
-	int N, Arry[1000], value, pointer{ 0 };
-	cin >> N;
-	for (int i = 0; i < N;i++) {
-		cin >> value;
-		if (i == 0) {
-			Arry[i] = value;
-		}
-		else if (value != Arry[pointer]) {
-			Arry[pointer+1] = value;
-			pointer++;
-		}
-	}
-	for (int j = 0; j < pointer ;j++) {
-		cout << Arry[j] << " ";
-	}
-}
->>>>>>> 77dda2487233aa1db96ddb792ecb1dbf0d4df575
